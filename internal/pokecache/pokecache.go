c7a7eb34426a2aa2433a44d837e9231ffd06e02f
nickcoates <nickdivineshine@gmail.com>
2026-05-09 12:12:43 +0100

    added cache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,52 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu      sync.Mutex
+	entries map[string]cacheEntry
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	t := &Cache{
+		entries: make(map[string]cacheEntry),
+	}
+	go t.reapLoop(interval)
+	return t
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	newEntry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.entries[key] = newEntry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	value, ok := c.entries[key]
+	return value.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.mu.Lock()
+		for key, entry := range c.entries {
+			if time.Now().Sub(entry.createdAt) > interval {
+				delete(c.entries, key)
+			}
+		}
+		c.mu.Unlock()
+	}
+}
